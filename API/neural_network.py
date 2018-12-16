6007e829f8c861da291f137658e45daffd5f5d3d
capimx <maucabamtz@gmail.com>
2018-12-16 04:42:08 +0000

    New nn

--- API/neural_network.py
+++ API/neural_network.py
@@ -13,30 +13,42 @@ class Neural_Network(nn.Module):
     #Definition.
     def __init__(self):
         super(Neural_Network, self).__init__()
-        
+
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=3, stride=1, padding=0)
-        
+        #fray 1
+        # 6 
         self.pool = nn.MaxPool2d(2, 2)
-        
-        self.fc1 = nn.Linear(16*5*5, 512)
+        self.dropout = nn.Dropout2d()
+
+        self.fc1 = nn.Linear(16*5*5, 512) #play   soft max
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 26)
-        
+
         self.Losses = []
         self.Accuracies = []
-     
-    #Forward function.    
+
+        
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    
+    #Forward function.
     def forward(self, x):
 
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16*5*5)
-        
+        x = x.view(-1, self.num_flat_features(x))
+
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x) 
-        
+        x = F.dropout(x, training=self.training)
+
+        x = self.fc3(x)
+
         return x 
     
     #Training function.
